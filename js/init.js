0a5d0342b00f22c5c922ab900a7cd8abbbae3b1b
SaulGomezReyes <87100704+SaulGomezReyes@users.noreply.github.com>
2021-07-07 18:55:14 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyDmovq7T_-Ym7iutIjJIN1ABI33usaMmJo",
-    authDomain: "autenfi41.firebaseapp.com",
-    projectId: "autenfi41",
-    storageBucket: "autenfi41.appspot.com",
-    messagingSenderId: "777550911881",
-    appId: "1:777550911881:web:205487691c96b111382958",
-    measurementId: "G-YHTXKL0597"
+   apiKey: "AIzaSyBTIUwRCN3py3glblV_pyfRkvaM4Z0sUvo",
+    authDomain: "autentificarsaul.firebaseapp.com",
+    projectId: "autentificarsaul",
+    storageBucket: "autentificarsaul.appspot.com",
+    messagingSenderId: "650510983357",
+    appId: "1:650510983357:web:636457b6fa7a9bd70557d4",
+    measurementId: "G-D5HNN42GX1"
+
 });
